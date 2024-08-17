3cd8b4b768fe38114dba4d82df7601d684a137f0
ArturShchekachPav <117753506+ArturShchekachPav@users.noreply.github.com>
2024-08-17 01:01:43 +0000

    feat: add routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,3 +1,7 @@
 const router = require('express').Router();
+const userRoutes = require('./user');
+const listRoutes = requere('./list');
+const noteRoutes = require('./note');
+const colorRoutes = require('./color');
 
 module.exports = router;
